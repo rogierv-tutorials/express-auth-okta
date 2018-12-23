ac75a1ad46ff3824c30289a28b6aad09c0a79e13
Rogier Verkaik <rogierv@gmail.com>
2018-12-23 22:33:22 +0100

    Connection with Okta, login, logout and register page added

---
+++ index.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(express.urlencoded({ extended: true }));
+app.use('/static', express.static('public'));
+
+// add auth middleware
+app.use(require('express-session')({
+  secret: process.env.APP_SECRET,
+  resave: true,
+  saveUninitialized: false
+}))
+
+const { ExpressOIDC } = require('@okta/oidc-middleware');
+const oidc = new ExpressOIDC({
+  issuer: `${process.env.OKTA_ORG_URL}/oauth2/default`,
+  client_id: process.env.OKTA_CLIENT_ID,
+  client_secret: process.env.OKTA_CLIENT_SECRET,
+  redirect_uri: `${process.env.HOST_URL}/authorization-code/callback`,
+  scope: 'openid profile'
+})
+
+app.use(oidc.router)
+
+// add registration page
+app.use('/register', require('./routes/register'));
+
+// add logout route
+
+app.get('/logout', (req, res) => {
+  if (req.userContext) {
+    const idToken = req.userContext.tokens.id_token
+    const to = encodeURI(process.env.HOST_URL)
+    const params = `id_token_hint=${idToken}&post_logout_redirect_uri=${to}`
+    req.logout()
+    res.redirect(`${process.env.OKTA_ORG_URL}/oauth2/default/v1/logout?${params}`)
+  } else {
+    res.redirect('/')
+  }
+})
+
+app.use('/', require('./routes/index'));
+
+const port = process.env.PORT || 3000
+app.listen(port, () => console.log(`App listening on port ${port}`));
